86fe8dcc1542e7c00ebc8c2b49a299625c19fcbb
Sakith Gunarathna <sakithmandira99@gmail.com>
2026-01-23 13:31:16 +0530

    add background cliper to project

---
+++ vango_driver_app/lib/core/widgets/background_cliper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height * 0.7); 
+    path.lineTo(size.width, size.height * 0.9); 
+    path.lineTo(size.width, 0); 
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
